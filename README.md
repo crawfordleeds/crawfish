c6a099168138cd397ebe9aa2fcd73f0c9bda3ff3
Crawford Leeds <crawford@crawfrdleeds.com>
2020-11-27 12:55:08 -0700

    improve setup.py

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # crawfish
+
+## Development
+
+Install package dependencies:
+
+```bash
+pip install -e .
+```
+
+Install dev dependencies:
+
+```bash
+pip install -e .[dev]
+```
--- setup.py
+++ setup.py
@@ -1,15 +1,51 @@
-from setuptools import find_packages, setup
+import os, io
+from setuptools import find_packages, setup, Command
+
+NAME = "django-crawfish"
+DESCRIPTION = "pending..."
+URL = "https://gitlab.com/crawfordleeds/crawfish"
+EMAIL = "crawford@crawfordleeds.com"
+AUTHOR = "Crawford Leeds"
+REQUIRES_PYTHON = ">=3.6.0"
+
+# Dont' manually change the version here. Use bumpversion instead
+VERSION = "0.0.0"
+
+# Required packages for this module
+REQUIRED = []
+
+# Optional packages
+EXTRAS = {"dev": ["black==20.8b1"]}
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long description
+try:
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = f"\n{f.read()}"
+except FileNotFoundError:
+    long_description = DESCRIPTION
 
 setup(
-    name="django-crawfish",
+    name=NAME,
+    version=VERSION,  # Don't change this manually, use bumpversion instead,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=["tests", "tests.*"]),
-    version="0.0.0",  # Don't change this manually, use bumpversion instead,
-    description="",
-    author="Crawford Leeds",
-    author_email="crawford@crawfordleeds.com",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    install_required=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license="MIT",
     classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience : Developers",
+        "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
-    ]
+    ],
 )
