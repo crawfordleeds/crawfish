6b676443bc43825483898efd9500ef0a88614f62
Crawford Leeds <crawford@crawfordleeds.com>
2020-11-27 18:13:37 -0700

    Bump version: 0.0.0 → 0.0.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ AUTHOR = "Crawford Leeds"
 REQUIRES_PYTHON = ">=3.6"
 
 # Dont' manually change the version here. Use bumpversion instead
-VERSION = "0.0.0"
+VERSION = "0.0.1"
 
 # Required packages for this module
 REQUIRED = ["Django>=2.2"]
