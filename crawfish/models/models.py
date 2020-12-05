96bf5e807bcac53d0401fba9be0a74ca37b12edb
Crawford Leeds <crawford@crawfordleeds.com>
2020-12-05 00:07:50 -0700

    Apply default datetime to created_at and updated_at datetimes in base model

--- crawfish/models/models.py
+++ crawfish/models/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, null=False)
-    updated_at = models.DateTimeField(auto_now=True, null=False)
+    created_at = models.DateTimeField(
+        auto_now_add=True, null=False, default=timezone.now()
+    )
+    updated_at = models.DateTimeField(auto_now=True, null=False, default=timezone.now())
 
     class Meta:
         abstract = True
