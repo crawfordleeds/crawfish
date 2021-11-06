ff7838a6b819e9cd1624c6008d7a898e5bff140b
Crawford Leeds <crawford@crawfordleeds.com>
2021-11-06 14:20:07 -0600

    add DataclassMappingMixin 💽

--- crawfish/mixins.py
+++ crawfish/mixins.py
@@ -1,7 +1,8 @@
 from rest_framework import exceptions as rest_exceptions
 
 from django.core.exceptions import ValidationError
-
+from dataclasses import _FIELD, _FIELDS, dataclass, fields
+from collections.abc import Mapping
 from .utils import get_error_message
 
 
@@ -25,3 +26,59 @@ class ApiErrorsMixin:
             return super().handle_exception(drf_exception)
 
         return super().handle_exception(exc)
+
+
+class DataclassMappingMixin(Mapping):
+    """
+    This mixin allows you to use a dataclass as a mapping.
+
+    Example:
+
+        @dataclass
+        class MyDataClass(DataclassMappingMixin):
+            field1: str
+            field2: str
+
+        my_data_class = MyDataClass(field1='value1', field2='value2')
+        my_data_class['field1']
+        my_data_class['field2']
+        my_data_class.__class__.__mro__
+        my_data_class.__class__.__name__
+        dict(my_data_class)
+        dict(**my_data_class)
+        fields(my_data_class)
+
+
+    This is useful as a data transfer object-like object with Django Rest Framework services following the
+    Hack Software Styleguide https://github.com/crawfordleeds/Django-Styleguide.
+
+    For example, if you were to create a service that updates a QuerySet, you could use this to specify and validate
+    the data acceptable by the service.
+
+
+        class UserFields(DataclassMappingMixin):
+            is_active: bool
+
+            def __post_init__(self):
+                # Here you can provide any additional validation required
+
+        def users_update(*, users: Optional["QuerySet[User]"] = None, fields: UserFields) -> int:
+
+            qs = users or get_user_model().objects.all()
+            return qs.update(**fields)
+
+        users_update(fields=UserFields(is_active=False))
+
+    """
+
+    def __iter__(self):
+        return (f.name for f in fields(self))
+
+    def __getitem__(self, key):
+        field = getattr(self, _FIELDS)[key]
+        if field._field_type is not _FIELD:
+            raise KeyError(f"'{key}' is not a dataclass field.")
+        return getattr(self, field.name)
+
+    def __len__(self):
+        return len(fields(self))
