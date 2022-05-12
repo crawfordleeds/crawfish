8b84336baa19fd645a6e5b1d16d001bfb1b267e1
Crawford Leeds <crawford@crawfordleeds.com>
2022-05-11 23:34:14 -0400

    remove comments

--- crawfish/mixins.py
+++ crawfish/mixins.py
@@ -1,7 +1,7 @@
 from rest_framework import exceptions as rest_exceptions
 
 from django.core.exceptions import ValidationError
-from dataclasses import _FIELD, _FIELDS, dataclass, fields
+from dataclasses import _FIELD, _FIELDS, fields
 from collections.abc import Mapping
 from .utils import get_error_message
 
@@ -20,13 +20,6 @@ class ApiErrorsMixin:
 
     def handle_exception(self, exc):
         return super().handle_exception(self.normalize_exception(exc))
-        # if isinstance(exc, tuple(self.expected_exceptions.keys())):
-        #     drf_exception_class = self.expected_exceptions[exc.__class__]
-        #     drf_exception = drf_exception_class(get_error_message(exc))
-        #
-        #     return super().handle_exception(drf_exception)
-        #
-        # return super().handle_exception(exc)
 
     @classmethod
     def normalize_exception(cls, exc):
